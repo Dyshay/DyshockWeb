3eef4b217a17fec34fb2f833580bba51a56c87a4
dys <dyl.crel@gmail.com>
2023-12-14 22:21:09 +0100

    Rework project with taildwin

--- src/app/pages/dashboard-page/dashboard-page.component.ts
+++ src/app/pages/dashboard-page/dashboard-page.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import {NavbarComponent} from "../../shared/components/navbar/navbar.component";
+import {SidebarComponent} from "../../shared/components/sidebar/sidebar.component";
+import {FooterComponent} from "../../shared/components/footer/footer.component";
 
 @Component({
   selector: 'app-dashboard-page',
   standalone: true,
-  imports: [],
+  imports: [
+    NavbarComponent,
+    SidebarComponent,
+    FooterComponent
+  ],
   templateUrl: './dashboard-page.component.html',
   styleUrl: './dashboard-page.component.scss'
 })

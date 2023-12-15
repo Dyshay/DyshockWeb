5caeea73dcca8d9e6aa93ca6622aa1455c800dc7
dys <dyl.crel@gmail.com>
2023-12-15 18:07:37 +0100

    Update css tweak / update

--- src/app/pages/dashboard-page/dashboard-page.component.ts
+++ src/app/pages/dashboard-page/dashboard-page.component.ts
@@ -4,6 +4,8 @@ import {SidebarComponent} from "../../shared/components/sidebar/sidebar.componen
 import {FooterComponent} from "../../shared/components/footer/footer.component";
 import {AdvertissementComponent} from "../../shared/components/advertissement/advertissement.component";
 import {ServerCardComponent} from "../../shared/server-card/server-card.component";
+import {ChatMessagesComponent} from "../../shared/components/chat-messages/chat-messages.component";
+import {CommonModule} from "@angular/common";
 
 @Component({
   selector: 'app-dashboard-page',
@@ -13,11 +15,14 @@ import {ServerCardComponent} from "../../shared/server-card/server-card.componen
     SidebarComponent,
     FooterComponent,
     AdvertissementComponent,
-    ServerCardComponent
+    ServerCardComponent,
+    ChatMessagesComponent,
+    CommonModule
   ],
   templateUrl: './dashboard-page.component.html',
   styleUrl: './dashboard-page.component.scss'
 })
 export class DashboardPageComponent {
 
+  isChatEnabled = false
 }
--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,17 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, signal} from '@angular/core';
 import {LinksComponent} from "../links/links.component";
 import {SteamAccountComponent} from "../steam-account/steam-account.component";
+import {NgClass} from "@angular/common";
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
   imports: [
     LinksComponent,
-    SteamAccountComponent
+    SteamAccountComponent,
+    NgClass
   ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  isMobileVisible = false
+
+  setMobileVisible(){
+    this.isMobileVisible = !this.isMobileVisible
+  }
 
 }
